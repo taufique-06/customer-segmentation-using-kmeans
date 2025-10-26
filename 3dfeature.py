d13bcfa14205b81ce48ac26cbcc61ff61fd5176d
Taufique Azad <85470428+taufique-06@users.noreply.github.com>
2025-10-26 18:59:31 +0000

    Update 3dfeature.py

--- 3dfeature.py
+++ 3dfeature.py
@@ -1,1 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [2,3,4]].values
+
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+kmeans = KMeans(n_clusters = 6, init = 'k-means++', random_state = 42)
+y_kmeans = kmeans.fit_predict(X)
+
+labels = kmeans.labels_
+centroids = kmeans.cluster_centers_
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection='3d')
+for i in range(5):
+    ax.scatter(X[kmeans.labels_ == i, 0], X[kmeans.labels_ == i, 1], X[kmeans.labels_ == i, 2], label=f'Cluster {i+1}')
+
+# Plot centroids
+ax.scatter(centroids[:, 0], centroids[:, 1], centroids[:, 2], s=200, c='black', marker='X', label='Centroids')
+
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
+ax.set_title('3D K-Means Clustering')
+ax.legend()
+plt.show()
